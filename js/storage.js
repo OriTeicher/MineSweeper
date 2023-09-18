d331ed86db404a6fa8197a850e9ff32e9b7902d0
OriTeicher <oriteicherudemy@gmail.com>
2023-09-18 21:21:16 +0300

    Responsive UI added

--- js/storage.js
+++ js/storage.js
@@ -14,7 +14,7 @@ function setGameTime() {
     var elBestTime = document.querySelector('.best-time span');
     var bestTime = Math.min(gBestTime, gGame.secsPassed);
     gBestTime = bestTime;
-    setLocalStorage("MinesweeperDB", (pName.length > 8) ? pName.slice(0, 8) + '..  .' : pName, gGame.secsPassed);
+    setLocalStorage("MinesweeperDB", (pName.length > 8) ? pName.slice(0, 8) + '..' : pName, gGame.secsPassed);
     elBestTime.innerText = ' ' + bestTime + 's';
 }
 
@@ -33,9 +33,10 @@ function renderScoreBoard() {
     var strHTML = '';
     gScores.sort((a, b) => a.score - b.score);
     for (var i = 0; i < gScores.length; i++) {
-        strHTML += '<tr>\n';
+        if (i === gScores.length - 1) strHTML += "<tr class='last-score'></tr>"
+        else strHTML += '<tr>\n';
         for (var j = 0; j < 1; j++) {
-            strHTML += `<td class="scoreboard-pos">${i + 1}</td><td class="scoreboard-cell name">${gScores[i].name}\n</td>
+            strHTML += `<td class="scoreboard-pos ${j === 0 ? 'gold' : j === 1 ? 'silver' : j === 2 ? 'bronze' : ''}">${i + 1}</td><td class="scoreboard-cell name">${gScores[i].name}\n</td>
             <td class="scoreboard-cell time">${gScores[i].score}s</td>\n`;
             strHTML += '</td>\n';
         }
@@ -54,8 +55,9 @@ function setDemoData() {
             { name: "Bruno G.", score: 124 },
             { name: "Messi", score: 102 },
             { name: "Ronaldo", score: 57 },
-            { name: "Harry Mag", score: 66 },
+            { name: "Harry", score: 66 },
         ];
+
         localStorage.setItem("MinesweeperDB", JSON.stringify(demoData));
     }
     createScoreBoard();
